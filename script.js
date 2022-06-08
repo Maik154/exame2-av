6eedbd973ca2718bdc79faf37f4e3371060e39c4
Miguel Blanco Guede <miguelblancoremo@gmail.com>
2022-06-08 19:10:39 +0200

    commit 2

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ async function cargarJSON() {
         const productos = await resul.json();
         console.log(productos);
         console.log(productos.productos);
-        mostrarProductos(productos);
+        mostrarProductos(productos.productos);
     } catch (error) {
         console.log(error);
     }
 }
 
 function mostrarProductos(productos) {
-    for (let i in Object.values(productos)[0]) {
+    for (let i in Object.values(productos)) {
         //JSON printeado tal cual
         document.getElementById("productos").innerHTML +=
-            `Nombre: ${Object.values(Object.values(productos)[0][i])[0]} -------
-         Precio: ${Object.values(Object.values(productos)[0][i])[1]}` + "<br>";
+            `Nombre: ${Object.values(productos)[i].nombre} -------
+         Precio: ${Object.values(productos)[i].precio}` + "<br>";
 
         //
         let div = document.getElementById("menuCompra");
@@ -28,9 +28,9 @@ function mostrarProductos(productos) {
         let txtCantidad = document.createElement("label");
         let inputCantidad = document.createElement("input");
         let btn = document.createElement("input");
-        nombre.append(Object.values(Object.values(productos)[0][i])[0]);
+        nombre.append(Object.values(productos)[i].nombre);
         div.appendChild(nombre);
-        precio.append(Object.values(Object.values(productos)[0][i])[1]);
+        precio.append(Object.values(productos)[i].precio);
         div.appendChild(precio);
         div.appendChild(separador);
         txtCantidad.append("Cantidad: ");
@@ -38,18 +38,21 @@ function mostrarProductos(productos) {
         div.appendChild(inputCantidad);
         inputCantidad.setAttribute("type", "number");
         inputCantidad.setAttribute("min", "0");
-        inputCantidad.setAttribute("id", 
-        "inputCantidad" + Object.values(Object.values(productos)[0][i])[0]);
+        inputCantidad.setAttribute("id",
+            "inputCantidad" + Object.values(productos)[i].nombre);
         btn.setAttribute("type", "submit");
         btn.setAttribute("value", "Comprar");
-        //btn.addEventListener("click", comprar(Object.values(Object.values(productos)[0][i])[0], i));
-        btn.addEventListener("click", comprar("manzanas", 1));
+        btn.addEventListener("click", comprar);
+        //btn.addEventListener("click", comprar("manzanas", 1));
+        let idBtnManzana = "btn" + Object.values(productos)[i].nombre;
+        btn.setAttribute("id", idBtnManzana);
         div.appendChild(btn);
     };
 }
 cargarJSON();
+//document.getElementById("btnManzana").addEventListener("click", comprar);
 
 
-function comprar(nombre, pos){
+function comprar(nombre, pos) {
     console.log(nombre, pos);
 }
